ceae6d60804837714f86c1a5641060779d274329
chris.hupman@gmail.com <chris.hupman@gmail.com>
2017-02-02 00:28:26 +0000

    added functions for adding tags and containers. Ready for testing

--- rtvmsync.py
+++ rtvmsync.py
@@ -15,7 +15,7 @@ from requests.auth import HTTPBasicAuth
 import pprint
 
 
-def GetArgs():
+def getArgs():
     """
     Supports the command-line arguments listed below.
     """
@@ -41,30 +41,40 @@ def GetArgs():
     return args
 
 
-def CreateObj(vmname, args):
+def createObj(vmname, args):
     addobj = "method=add_object"
     type = "&object_type_id=1504"
     objname = "&object_name=" + vmname
     url = args.api + addobj + type + objname
+    res = requests.get(url, auth=HTTPBasicAuth(args.user, args.password))
+    code = res.status_code
+    if code == 200:
+        print("Created object " + vmname + " successfully")
+    # objret = res
+    # return objret
 
 
-def AddTags(vmname, id, taglist, args):
+def addTags(id, taglist, args):
     addtags = "method=update_object_tag"
     object_id = "&object_id=" + id
     tags = "&taglist=" + taglist
     url = args.api + addtags + object_id + tags
 
 
-def AddContainer(vmname, id, cluster_id, args):
+def addContainer(id, cluster_id, args):
     addcontainer = "method=link_entities"
     chtype = "&child_entity_type=object"
     chid = "&child_entity_id=" + id
     partype = "&parent_entity_type=object"
     parid = "&parent_entity_id=" + cluster_id
     url = args.api + addcontainer + chtype + chid + partype + parid
+    res = requests.get(url, auth=HTTPBasicAuth(args.user, args.password))
+    code = res.status_code
+    if code == 200:
+        print("Added Container successfully")
 
 
-def GetRTData(args):
+def getRTData(args):
     # Connect to racktables and return requested data as json
     depot = "method=get_depot"
     exp = "&andor=and&cft%5B%5D=15&cfe=%7B%24typeid_1504%7D&include_attrs=1"
@@ -74,7 +84,7 @@ def GetRTData(args):
     return rtdata
 
 
-def GetDiff(vmdata, rtdata, args):
+def getDiff(vmdata, rtdata, args):
     # Get vm names of systems already in racktables
     rtlist = []
     vmlist = []
@@ -86,29 +96,70 @@ def GetDiff(vmdata, rtdata, args):
         rtdict[name]["parent_type_id"] = val["container_objtype_id"]
         rtdict[name]["cluster"] = val["container_name"]
         rtdict[name]["tags"] = val["itags"]
-        rtdict[name]["project"] = val["etags"]  # nested by project tag ids with info nested
-        rtdict[name]["ips"] = val["ipv4"]
+        rtdict[name]["project"] = val["etags"]  # Project tag ids nested
+        eth = 0
+        rtdict[name]["ips"] = {}
+        for ipbin, v in val["ipv4"].iteritems():
+            # pprint.pprint(val["ipv4"][ipbin])
+            addr = val["ipv4"][ipbin]["addrinfo"]["ip"]
+            rtdict[name]["ips"][eth] = {"ip": addr}
+            eth += 1
         rtdict[name]["id"] = val["id"]
     # Get vm names of
     for vmname, attrs in vmdata.iteritems():
-        vmlist.append(vmname)  # add vm names into a list
+        if attrs["state"] == "poweredOn":  # Only add powered on VMs
+            vmlist.append(vmname)  # Add vm names into a list
 
     match = set(vmlist).intersection(rtlist)  # VMs that exist in both systems
     diff = set(vmlist).difference(rtlist)  # VMs that need to be added
 
-    if not args.silent:
-        print("Match:")
-        print(list(match))
-        print("Diff:")
-        print(list(diff))
     for vmname in list(diff):
-        CreateObj(vmname, args)
-    pprint.pprint(rtdict)
+        createObj(vmname, args)
+    # pprint.pprint(rtdict)
+    return diff, match, rtdict
+
+
+def getClusterIDs(cluster, clusterlist):
+    pass
+
+
+def getProjectTags(args):
+    # depot = "method=get_depot"
+    # exp = "&andor=and&cft%5B%5D=15"
+    taglist = "method=get_taglist"
+    # url = args.api + depot + exp
+    url = args.api + taglist
+    res = requests.get(url, auth=HTTPBasicAuth(args.user, args.password))
+    tagtree = res.json()
+    projectTags = {}
+    # Iterate through returned json and restructure data
+    for k, v in tagtree["response"].iteritems():
+        # Only populate if it's under the projects tag umbrella
+        if tagtree["response"][k]["trace"]["0"] == "15":
+            projectName = tagtree["response"][k]["tags"]
+            projectID = tagtree["response"][k]["id"]
+            projectTags[projectName] = projectID
+    return projectTags
+
+
+def getClusterList(args):
+    depot = "method=get_depot"
+    exp = "&andor=and&cfe=%7B%24typeid_1505%7D"
+    url = args.api + depot + exp
+    res = requests.get(url, auth=HTTPBasicAuth(args.user, args.password))
+    clusters = res.json()
+    clusterDict = {}
+    # Iterate through returned json and restructure data
+    for k, v in clusters["response"].iteritems():
+        clusterName = clusters["response"][k]["name"]
+        id = clusters["response"][k]["id"]
+        clusterDict[clustername] = id
+    return clusterDict
 
 
 def main():
 
-    args = GetArgs()
+    args = getArgs()
 
     if args.password:
         password = args.password
@@ -119,11 +170,26 @@ def main():
     with open(args.jsonfile) as json_file:
         vmdata = json.load(json_file)
 
-    rtdata = GetRTData(args)
+    rtdata = getRTData(args)
+    projectTags = getProjectTags(args)
+    pprint.pprint(tagtree)
+    clusterDict = getClusterList(args)
+    pprint.pprint(clusterlist)
+    diff, match, rtdict = getDiff(vmdata, rtdata, args)
 
-    GetDiff(vmdata, rtdata, args)
-
-
+    if not args.silent:
+        print("Match:")
+        print(list(match))
+        print("Diff:")
+        print(list(diff))
+    for vm in diff:
+        taglist = []
+        id = rtdict[vm]["id"]
+        taglist.appent(projectTags[vmdata[vm]["folder"]])
+        cluster_id = clusterDict[vmdata[vm]["cluster"]]
+        print(taglist)
+        addTags(id, taglist, args)
+        addContainer(id, cluster_id, args)
 # Start program
 if __name__ == "__main__":
     main()
